ce9e934aff295057d8c95db97c83ee89ed1a589c
Tony Knudson <tonyknudson91@gmail.com>
2024-01-13 23:28:00 -0600

    added field validation, display errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to articles_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
     if @article.update(article_params)
       redirect_to articles_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
